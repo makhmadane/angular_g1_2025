76e190482727a309b41b0ca8c648b1e02635899c
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2025-05-05 20:12:24 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'offre_l3gl_angular';
+}
